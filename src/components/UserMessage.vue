62bf275f69634126bed5ae2f49d7dcbaae50f3a4
LuborKoka <koka.lubor87@gmail.com>
2023-10-08 18:52:27 +0200

    Create UserMessage.vue

---
+++ src/components/UserMessage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="bg-secondary rounded-borders q-py-xs q-px-sm white-text q-ma-xs inline-block"
+  >
+    <b> {{ sender }}</b
+    >: <span> {{ content }}</span>
+  </div>
+  <br />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'UserMessage',
+  props: {
+    sender: {
+      type: String,
+      default: 'XD?',
+    },
+    content: {
+      type: String,
+      default: 'XDD',
+    },
+  },
+});
+</script>
+
+<style scoped>
+.inline-block {
+  display: inline-block;
+}
+</style>
