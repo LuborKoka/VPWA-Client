59402d64389c206f21aa9f0dd3513adb37f15417
LuborKoka <koka.lubor87@gmail.com>
2023-10-08 18:52:10 +0200

    channel nav link

---
+++ src/components/ChannelNav.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-item clickable>
+    <router-link
+      class="text-primary text-bold full-width q-pa-md text-decoration-none display-block"
+      :to="{ path: '/', query: { channelName: encodeURIComponent(title) } }"
+    >
+      # {{ title }}
+    </router-link>
+  </q-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'ChannelNav',
+  props: {
+    title: {
+      type: String,
+      default: 'Channel Name',
+    },
+  },
+});
+</script>
