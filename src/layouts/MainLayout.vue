2cbad3a6ddd49380796c3676198cc75376826c09
LuborKoka <koka.lubor87@gmail.com>
2023-10-08 18:51:22 +0200

    basic layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,117 +1,52 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
     <q-drawer
       v-model="leftDrawerOpen"
       show-if-above
       bordered
+      content-class="bg-grey-1"
     >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+      <channel-nav title="Channel 1"></channel-nav>
+      <channel-nav title="Channel 2"></channel-nav>
+      <channel-nav title="XDDD"></channel-nav>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-drawer
+      v-model="rightDrawerOpen"
+      show-if-above
+      side="right"
+      bordered
+      content-class="bg-grey-1"
+    >
+      <available-user status="online" username="Peter Paprika"></available-user>
+      <available-user status="dnd" username="Samo Chladnička"></available-user>
+      <available-user
+        status="offline"
+        username="Štefan Nátierka"
+      ></available-user>
+    </q-drawer>
   </q-layout>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import EssentialLink from 'components/EssentialLink.vue';
+<script>
+import ChannelNav from 'src/components/ChannelNav.vue';
+import AvailableUser from 'src/components/AvailableUser.vue';
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
-export default defineComponent({
-  name: 'MainLayout',
-
+export default {
   components: {
-    EssentialLink
+    ChannelNav,
+    AvailableUser,
   },
 
   data() {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksList
-    }
+      leftDrawerOpen: true,
+      rightDrawerOpen: false,
+    };
   },
-
-  methods: {
-    toggleLeftDrawer () {
-      this.leftDrawerOpen = !this.leftDrawerOpen
-    }
-  }
-});
+};
 </script>
