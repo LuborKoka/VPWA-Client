d3971494d82d432e7d76670c0392c49c2cfe8824
Adrián Hládek <71824498+Adrian-Hladek@users.noreply.github.com>
2023-10-29 16:45:06 +0100

    Popup okno done
    do poupu s abude moc5 pridava5 toho viac ale ni4 ine ma ATM nenapadlo

--- src/components/AvailableUser.vue
+++ src/components/AvailableUser.vue
@@ -10,9 +10,36 @@
           ></q-icon>
         </q-item-section>
         <q-item-section>
-          <q-item-label> {{ username }}</q-item-label>
+          <q-item-label > {{ username }}</q-item-label>
         </q-item-section>
       </q-item>
+      <q-popup-proxy>
+        <div class="q-pa-md q-gutter-y-md"
+        style="min-height: 100px; min-width: 265px;" align="left">
+
+
+        
+        <q-item-section side>
+        <q-avatar rounded size="96px">
+          <img src="https://cdn.quasar.dev/img/avatar.png" />
+          
+        </q-avatar>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label style="font-size: 24px;">
+
+          {{ username }}
+        </q-item-label>
+        <q-item-label >
+          <q-badge class="custom-badge" style="font-size: 20px;" :color="statusColor"> {{ formattedStatus }}</q-badge>
+        </q-item-label>
+        
+        <q-item-label caption>235 messages</q-item-label>
+        <q-item-label caption>Member since: {{ currentDate }} </q-item-label>
+      </q-item-section>
+
+      </div>
+      </q-popup-proxy>
     </q-btn>
   </div>
 </template>
@@ -34,12 +61,51 @@ export default defineComponent({
         return ['online', 'dnd', 'offline'].includes(value);
       },
     },
+    
+    
+
+
   },
+
+  data() {
+    return {
+      currentDate: new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })
+    };
+  },
+  
+  computed: {
+    statusColor() {
+      // Define a function to map status text to colors
+      const statusColorMap = {
+        online: 'green', // Customize colors as needed
+        offline: 'grey',
+        dnd: 'red',
+      };
+      // Use the map to get the color
+      return statusColorMap[this.status] || 'default-color';
+    },
+
+    formattedStatus() {
+      // Define a map for formatting status text
+      const statusTextMap = {
+        online: 'Online',
+        offline: 'Offline',
+        dnd: 'Do not Disturb',
+      };
+      return statusTextMap[this.status] || this.status; // Use the formatted text or original status
+    },
+
+  },
+  
 
   methods: {
     emitClick() {
       this.$emit('click', this.username, this.status);
     },
+
+   
+
+
   },
 });
 </script>
@@ -94,4 +160,11 @@ export default defineComponent({
   text-align: left; // Adjust the alignment as needed
   width: 100%; // Make the button fill the available width
 }
+
+.custom-badge {
+  font-size: 28px; /* Adjust the size as needed */
+  padding: 5px 10px; /* Adjust the padding as needed */
+  border-radius: 20px; /* Makes it rounder */
+}
+
 </style>
