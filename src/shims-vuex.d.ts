569d0a04dd779e5e4c038518aea6d53449878114
LuborKoka <koka.lubor87@gmail.com>
2023-11-19 17:49:35 +0100

    Delete shims-vuex.d.ts

--- src/shims-vuex.d.ts
+++
@@ -1,9 +0,0 @@
-import { Store } from 'vuex';
-import { IStoreState } from './store';
-
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $store: Store<IStoreState>;
-  }
-}
-
